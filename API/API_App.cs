b434679f68a0efb488eacf7c9a1dedc9a3067294
Paweł Niedziółka <249022@student.pwr.edu.pl>
2021-04-08 21:59:15 +0200

    v2.0

--- API/API_App.cs
+++ API/API_App.cs
@@ -19,7 +19,7 @@ namespace API
         {
             InitializeComponent();
             label2.Text = "symbol";
-            label3.Text = ""; 
+            label3.Text = "";
         }
 
         public async void last_deleted()
@@ -31,7 +31,7 @@ namespace API
                 RequestUri = new Uri("https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?t=deleted&cl=" + symbol + "&st=1"),
                 //RequestUri= new Uri("https://numbersapi.p.rapidapi.com/6/21/date?fragment=true&json=true"),
 
-            Headers =
+                Headers =
                 {
                     { "x-rapidapi-key", "bb39cfd0f9mshfa3efd401f9eb7cp12860djsn6e8a143a24f8" },
                     { "x-rapidapi-host", "unogs-unogs-v1.p.rapidapi.com"  },
@@ -48,7 +48,7 @@ namespace API
                 var read = body.Split("\"");
                 //richTextBox1.Text = body;
                 if (read[3] == "0") richTextBox1.Text = "Brak wyników w bazie!";
-                else richTextBox1.Text ="Netflix ID: " + read[9] + "\nTytuł: " + read[13] + "\nData usunięcia: " + read[21];
+                else richTextBox1.Text = "Netflix ID: " + read[9] + "\nTytuł: " + read[13] + "\nData usunięcia: " + read[21];
 
 
                 //{"COUNT":"1","ITEMS":[{"netflixid":"60000861","title":"American Psycho",
@@ -58,6 +58,7 @@ namespace API
 
         public async void DB_expiring()
         {
+            var context = new MOVIE_DB_Context();
             var client = new HttpClient();
             var request = new HttpRequestMessage
             {
@@ -85,12 +86,19 @@ namespace API
                     {
                         int j = i * 48;
                         var description = read[j + 21].Split("<");
+                        context.Movies.Add(new Movie { ID = read[j + 9], title = read[j + 13], description = description[0], premiere = read[j + 33], date_exp = read[j + 45] });
                         richTextBox2.Text = "Netflix ID: " + read[j + 9] + "\nTytuł: " + read[j + 13] + "\nOpis: " + description[0] + "\nRok premiery: " + read[j + 33] + "\nData wygasniecia filmu: " + read[j + 45];
                     }
                 }
-                                                                                                    //download
-                                                         //57 nowa linia                                                                                 //read[51] +
-                                                         // i= +48                                                                
+                // wysrywanie tego całehgo gówna
+
+                var students = (from s in context.Students select s).ToList<Student>();
+                foreach (var st in students)
+                {
+                    Console.WriteLine("ID: {0}, Name: {1}, Avg: {2:0.00}", st.ID, st.Name, st.Avg);
+                }
+
+
                 /*
                  * {"COUNT":"22","ITEMS":[{"netflixid":"80100648","title":"The Infiltrator","image":"https://occ-0-2773-2774.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABbto1zY2QhLF7uva7FMogD_oW0oFSBVuPxayhUJS4-JoJo1feknVHk6RpzB-hJrrFjVwoVw9apl8PpheiBj4fVjFMQ.jpg?r=d3c","synopsis":"A canny federal agent navigates a deadly world in which one mistake could get him killed: the criminal underworld of Colombia, ruled by Pablo Escobar.<br><b>Expires on 2021-04-08</b>","rating":"7","type":"movie","released":"2016","runtime":"2h6m","largeimage":"https://occ-0-114-116.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABSCw_hM_WWEaQE0P9a3zQxNLJ3TfRUwzD8jfGdf-qsvQL1a7KxHObZDIyLcY9zL2gvQAuWiIjqq4WPOS57xk15TY3d-h.jpg?r=7b6","unogsdate":"2021-04-08","imdbid":"tt1355631","download":"1"},
                  * {"netflixid":"80036832","title":"How to Change the World","image":"https://occ-0-2717-360.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABYtukAQro9yQfGCsTcnyfV9rtMnt9YuvdxmbJe5Vgc3F5NaCoA1x6xt2D6gw0qEPfMNKdjUjbi8nHBbLfHe74o0K6Q.jpg?r=bcb","synopsis":"In the 1970s, a group of activists who gathered to protest nuclear testing formed the iconic Greenpeace environmental organization.<br><b>Expires on 2021-04-09</b>","rating":"7.6","type":"movie","released":"2015","runtime":"1h50m","largeimage":"https://occ-0-1068-92.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABV71SZ3lvUVyTuCI-t-Renv0F55kA0aN5hA6JhCbgle_MDzY3UGlhApRLbkyd9Wxm7llNa92IVM7iHkEOb1WzQq-PUE9.jpg?r=784","unogsdate":"2021-04-09","imdbid":"tt4144504","download":"0"}
