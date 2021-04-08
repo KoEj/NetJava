715dc4bc74f30d1ff1fbcce56639cdb47e02da3d
Paweł Niedziółka <249022@student.pwr.edu.pl>
2021-04-08 21:48:20 +0200

    v1.9

--- API/Baza_danych.cs
+++ API/Baza_danych.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data.Entity;
 
-namespace Zadanie2
+namespace API
 {
-    class Baza_danych
+    public class Movie
     {
+
+        //Opis: " + description[0] + "
+        //\nRok premiery: " + read[j + 33] + "\n
+        //Data wygasniecia filmu: " + read[j + 45];
+        public int ID { set; get; }
+        public string title { set; get; }
+        public string description { set; get; }
+        public int premiere { set; get; }
+        public string date { set; get; }
+    }
+
+    public class MOVIE_DB_Context : DbContext
+    {
+        public virtual DbSet<Movie> Movies { get; set; }
     }
 }
+
